d6ab768e4ea722eec36b64a993634254625fcd07
Andrew Parry <2393265p@student.gla.ac.uk>
2023-02-24 12:14:17 +0000

    Small changes to args in snippet

--- pyterrier_summary/util.py
+++ pyterrier_summary/util.py
@@ -10,24 +10,44 @@ acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
 websites = "[.](com|net|org|io|gov|edu|me)"
 digits = "([0-9])"
 
-def sentencesnippets(
+def sentence_snippets(
         text_scorer_pipe : pt.Transformer, 
         text_attr : str = "text", 
-        summary_attr : str = "summary") -> pt.Transformer:
+        summary_attr : str = "summary", 
+        num_psgs : int = None, 
+        joinstr : str = None) -> pt.Transformer:
     """
     Applies query-biased summarisation (snippet), by applying the specified text scoring pipeline.
 
     Parameters:
         text_scorer_pipe(Transformer): the pipeline for scoring passages in response to the query. Normally this applies passaging.
         text_attr(str): what is the name of the attribute that contains the text of the document
+        summary_attr(str): what is the name of the attribute that should contain the query-biased summary for that document
+        num_psgs(int): how many passages to select for the summary of each document
+        joinstr(str): how to join passages for a given document together
+
+    Example::
+
+        # retrieve documents with text
+        br = pt.BatchRetrieve(index, metadata=['docno', 'text'])
+
+        # use Tf as a passage scorer on sliding window passages 
+        psg_scorer = ( 
+            pt.text.sliding(text_attr='text', length=15, prepend_attr=None) 
+            >> pt.text.scorer(body_attr="text", wmodel='Tf', takes='docs')
+        )
+        
         # use psg_scorer for performing query-biased summarisation on docs retrieved by br 
         retr_pipe = br >> pt.text.snippets(psg_scorer)
 
     """
+    import pyterrier as pt
+
+    scorer = text_scorer_pipe % num_psgs if num_psgs else text_scorer_pipe
     tsp = (
         pt.apply.rename({'qid' : 'oldqid'}) 
         >> pt.apply.qid(lambda row: row['oldqid'] + '-' + row['docno']) 
-        >> text_scorer_pipe
+        >> scorer
         >> pt.apply.qid(drop=True)
         >> pt.apply.rename({'oldqid' : 'qid'})
     )
@@ -47,7 +67,9 @@ def sentencesnippets(
 
         psgres[["olddocno", "pid"]] = psgres.docno.str.split("%p", expand=True)
 
-        newdf = psgres.groupby(['qid', 'olddocno'])[text_attr].reset_index().rename(columns={text_attr : summary_attr, 'olddocno' : 'docno'})
+        new_text = psgres.groupby(['qid', 'olddocno'])[text_attr]
+        if joinstr: new_text = new_text.agg(joinstr.join)
+        newdf = new_text.reset_index().rename(columns={text_attr : summary_attr, 'olddocno' : 'docno'})
         
         return docres.merge(newdf, on=['qid', 'docno'], how='left')
     return pt.apply.generic(_qbsjoin)   
@@ -86,5 +108,5 @@ def splitter(df, body_attr='text'):
     return df
 
 def compose_pipe(data_pipe, summarizer):
-    summary = sentencesnippets(summarizer, text_attr=summarizer.body_attr, summary_attr='summary') 
+    summary = sentence_snippets(summarizer, text_attr=summarizer.body_attr, summary_attr='summary') 
     return data_pipe >> summary 
