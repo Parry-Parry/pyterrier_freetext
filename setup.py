5a71c668ad2b095bfaa217dcac065cf1b58cc098
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-10 12:54:26 +0000

    Updating setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyterrier-summary",
-    version="0.0.1",
+    name="pyterrier-freetext",
+    version="0.0.2",
     author="Andrew Parry",
     author_email='a.parry.1{at}.research.gla.ac.uk',
-    description="PyTerrier components for Text Summarisation",
+    description="PyTerrier components for free text manipulation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
