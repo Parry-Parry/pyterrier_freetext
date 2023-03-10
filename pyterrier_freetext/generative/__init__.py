4f736c0002f5bd29782d744d7a2971eb42a5c649
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-10 12:46:18 +0000

    Refactor and LLM addition

---
+++ pyterrier_freetext/generative/__init__.py
@@ -0,0 +1,86 @@
+import pyterrier as pt
+import re
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+class GenerativeTransformer(pt.Transformer):
+    def __init__(self, 
+                 prompt, 
+                 model_id, 
+                 out_attr='text', 
+                 post_process=lambda x : x,
+                 **kwargs) -> None:
+        super().__init__()
+        self.prompt = prompt 
+        self.out_attr = out_attr 
+        self.post_process = post_process
+        self.batch_size = kwargs.pop('batch_size', 1)
+        
+        self.tokenizer = AutoTokenizer(model_id)
+        
+        do_int8 = kwargs.pop('do_int8', False)
+        low_cpu_mem_usage = kwargs.pop('low_cpu_mem_usage', False)
+        device_map = kwargs.pop('device_map', 'auto')
+
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map=device_map,
+            torch_dtype=torch.int8 if do_int8 else torch.float16,
+            low_cpu_mem_usage=True if low_cpu_mem_usage else None,
+            load_in_8bit=do_int8
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast="/opt" not in model_id)
+
+        self.generate_kwargs = {
+            "max_new_tokens": kwargs.pop('max_tok', 128),
+            "min_new_tokens": kwargs.pop('max_tok', 0),
+            "temperature": kwargs.pop('temperature', 0.8),
+            "do_sample": kwargs.pop('constrastive_search', False), 
+            "top_k": kwargs.pop('top_k', 5),
+            "penalty_alpha": kwargs.pop('penalty_alpha', 0.6),
+            "repetition_penalty": kwargs.pop('repetition_penalty', 1.0),
+            "length_penalty" : kwargs.pop('length_penalty', 1.0)
+        }
+    
+    def fit(self, train_data) -> None:
+        raise NotImplementedError
+
+    def generate(self, frame) -> str:
+        sub = frame[self.prompt.get_params()].todict()
+        text = self.prompt(**sub)
+        with torch.no_grad():
+            input_ids = self.tokenizer(text, return_tensors="pt").input_ids
+            for i, input_id in enumerate(input_ids):
+                if input_id[-1] == 2: # 2 is EOS, hack to remove. If the prompt is ending with EOS, often the generation will stop abruptly.
+                    input_ids[i] = input_id[:-1]
+            input_ids = input_ids.to(0)
+        
+            generated_ids = self.model.generate(
+                input_ids,
+                **self.generate_kwargs
+            )
+            return self.post_process(self.tokenizer.batch_decode(generated_ids.cpu(), skip_special_tokens=True)[0])
+
+    def transform(self, input):
+        output = input.copy()
+        output[self.out_attr] = output.apply(lambda x : self.generate(x))
+
+class prompt_constructor:
+    def __init__(self, prompt, params) -> None:
+        for param in params: assert f"[{param}]" in prompt, "Parameter not found in prompt"
+        self.prompt = prompt
+        self.params = params 
+    
+    def get_params(self):
+        return self.params
+        
+    def create_prompt(self, **kwargs):
+        tmp_prompt = self.prompt
+        if self.names:
+            params = {param : kwargs.pop(param, "") for param in self.params}
+            for param in self.names: tmp_prompt = re.sub(f'[{param}]', tmp_prompt, params[param]) 
+        return tmp_prompt
+
+def generic_transform(prompt, model_id, **kwargs):
+    transform = GenerativeTransformer(prompt, model_id, **kwargs)
+    return pt.generic_apply(lambda x : transform(x))
---
+++ pyterrier_freetext/util.py
@@ -0,0 +1,129 @@
+import pyterrier as pt
+import re
+from typing import List 
+
+import torch
+from transformers import AutoModelForCausalLM, AutoConfig
+from accelerate import init_empty_weights, infer_auto_device_map
+
+
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov|edu|me)"
+digits = "([0-9])"
+
+def sentence_snippets(
+        text_scorer_pipe : pt.Transformer, 
+        text_attr : str = "text", 
+        summary_attr : str = "summary", 
+        num_psgs : int = None, 
+        joinstr : str = None) -> pt.Transformer:
+    """
+    Applies query-biased summarisation (snippet), by applying the specified text scoring pipeline.
+
+    Parameters:
+        text_scorer_pipe(Transformer): the pipeline for scoring passages in response to the query. Normally this applies passaging.
+        text_attr(str): what is the name of the attribute that contains the text of the document
+        summary_attr(str): what is the name of the attribute that should contain the query-biased summary for that document
+        num_psgs(int): how many passages to select for the summary of each document
+        joinstr(str): how to join passages for a given document together
+
+    Example::
+
+        # retrieve documents with text
+        br = pt.BatchRetrieve(index, metadata=['docno', 'text'])
+
+        # use Tf as a passage scorer on sliding window passages 
+        psg_scorer = ( 
+            pt.text.sliding(text_attr='text', length=15, prepend_attr=None) 
+            >> pt.text.scorer(body_attr="text", wmodel='Tf', takes='docs')
+        )
+        
+        # use psg_scorer for performing query-biased summarisation on docs retrieved by br 
+        retr_pipe = br >> pt.text.snippets(psg_scorer)
+
+    """
+
+    scorer = text_scorer_pipe % num_psgs if num_psgs else text_scorer_pipe
+    tsp = (
+        pt.apply.rename({'qid' : 'oldqid'}) 
+        >> pt.apply.qid(lambda row: row['oldqid'] + '-' + row['docno']) 
+        >> scorer
+        >> pt.apply.qid(drop=True)
+        >> pt.apply.rename({'oldqid' : 'qid'})
+    )
+
+    def _qbsjoin(docres):
+        import pandas as pd
+        if len(docres) == 0:
+            docres[summary_attr] = pd.Series(dtype='str')
+            return docres     
+
+        psgres = tsp(docres)
+        if len(psgres) == 0:
+            print('no passages found in %d documents for query %s' % (len(docres), docres.iloc[0].query))
+            docres = docres.copy()
+            docres[summary_attr]  = ""
+            return docres
+
+        psgres[["olddocno", "pid"]] = psgres.docno.str.split("%p", expand=True)
+
+        new_text = psgres.groupby(['qid', 'olddocno'])[text_attr]
+        if joinstr: new_text = new_text.agg(joinstr.join)
+        newdf = new_text.reset_index().rename(columns={text_attr : summary_attr, 'olddocno' : 'docno'})
+        
+        return docres.merge(newdf, on=['qid', 'docno'], how='left')
+    return pt.apply.generic(_qbsjoin)   
+
+def split_into_sentences(text : str) -> List[str]:
+    """Sentence Regex from: https://stackoverflow.com/a/31505798"""
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    if "..." in text: text = text.replace("...","<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+def splitter(df, body_attr='text'):
+    df['sentences'] = df[body_attr].apply(lambda x : split_into_sentences(x), axis=1)
+    return df
+
+def compose_pipe(data_pipe, summarizer):
+    summary = sentence_snippets(summarizer, text_attr=summarizer.body_attr, summary_attr='summary') 
+    return data_pipe >> summary 
+
+def get_map(model_id : str, mem : dict = None, do_int8 : bool = True):
+    if not mem: return 'auto'
+    with init_empty_weights():
+        config = AutoConfig.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_config(config)
+    
+    device_map = infer_auto_device_map(
+        model, max_memory=mem, dtype=torch.int8 if do_int8 else torch.float16, no_split_module_classes=["BloomBlock", "OPTDecoderLayer", "LLaMADecoderLayer"]
+    )
+    print(device_map)
+    del model 
+    return device_map
